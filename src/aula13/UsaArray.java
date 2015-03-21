e5404da584b55b0a8356283c4ea28c1efd1b8aa8
Renato Filho <renatosousafilho@gmail.com>
2015-03-21 00:34:37 -0300

    aula 12

---
+++ src/aula13/UsaArray.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author renato
+ */
+public class UsaArray {
+    public static void main(String[] args) {
+        
+        
+        List vingadores = new ArrayList();
+        vingadores.add("Homem de Ferro");
+        vingadores.add("Cap America");
+        System.out.println(vingadores.get(0));
+        System.out.println(vingadores.get(1));
+        vingadores.remove(0);
+        System.out.println(vingadores.get(0));
+        System.out.println(vingadores.get(1));
+        vingadores.set(index, args)
+        
+    }
+}
